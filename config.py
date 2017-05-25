0adecb7c0989ef74f7ae2a81e3320e4ec941bfd5
kirill <beelzebul.18@gmail.com>
2017-05-25 16:55:34 +0300

    UP

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 API_TOKEN = "243698434:AAHp3srhiwNhzD_IL2hOw7mv2XiRl3JJxb0"
 currency = "$"
 db = {
-    "database_name":"test",
-    "user":"qtiicygq",
-    "password":"SuAUlShAAZHDwZi9ZjNma0_6mFkT5vC6",
-    "host":"stampy.db.elephantsql.com"
+    "database_name":"d5duqosbv0h49h",
+    "user":"rlxtnrpvfsdcrk",
+    "password":"f6cc8cde46b431ea09b4d987184ab66412b92d5e0f9bafc7493943fef3909667",
+    "host":"ec2-23-23-234-118.compute-1.amazonaws.com"
 }
 
 
